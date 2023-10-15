c4fa9d66b2deb384ad0c4780bd828fb3bf07f491
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2023-10-15 19:47:34 +0500

    🔧update => image-upload component

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -23,12 +23,11 @@ import {
 } from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
 import {AlertModal} from "@/components/modals/alert-modal";
-import {ApiAlert} from "@/components/ui/api-alert";
-import {useOrigin} from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
     label: z.string().min(1),
-    imgUrl: z.string().min(1),
+    imageUrl: z.string().min(1),
 });
 
 type BillboardFormValues = z.infer<typeof formSchema>;
@@ -40,7 +39,6 @@ interface BillboardFormProps {
 export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
     const params = useParams();
     const router = useRouter();
-    const origin = useOrigin();
 
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
@@ -54,7 +52,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
             label: "",
-            imgUrl: "",
+            imageUrl: "",
         },
     });
 
@@ -110,6 +108,24 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
             <Separator />
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Background Image</FormLabel>
+                                <FormControl>
+                                    <ImageUpload
+                                        disable={loading}
+                                        onChange={(url) => field.onChange(url)}
+                                        onRemove={() => field.onChange("")}
+                                        value={field.value? [field.value] : []}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
                     <div className="grid grid-cols-3 gap-8">
                         <FormField
                             control={form.control}
