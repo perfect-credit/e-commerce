7ed49f7ffc66b16eae719fea9e650ddf269317c4
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2023-10-15 18:24:57 +0500

    🔧update => image-upload component

--- ecommerce-admin/components/ui/image-upload.tsx
+++ ecommerce-admin/components/ui/image-upload.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
+import {Trash} from "lucide-react";
+
+import {Button} from "@/components/ui/button";
 
 interface ImageUploadProps {
     disable: boolean;
@@ -16,11 +19,25 @@ const ImageUpload: React.FC<ImageUploadProps> = ({disable, onChange, onRemove, v
         setIsMounted(true);
     }, []);
 
+    const onUpload = (result: any) => {
+        onChange(result.info.secure_url);
+    };
+
     if (!isMounted) return null;
 
     return (
         <div>
-            <h1>Image Upload</h1>
+            <div className="mb-4 flex items-center gap-4">
+                {value.map((url, index) => (
+                    <div key={index} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
+                        <div className="z-10 absolute top-2 right-2">
+                            <Button type="button" onClick={() => onRemove(url)}>
+                                <Trash className="h-4 w-4" />
+                            </Button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
