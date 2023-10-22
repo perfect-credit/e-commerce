8a0aa56a29bf0f25e7a41357883736760f5dc3bf
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2023-10-22 18:39:09 +0500

    ✨add => datatable

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import {Plus} from "lucide-react";
+import React from "react";
 import {useRouter, useParams} from "next/navigation";
+import {Plus} from "lucide-react";
 
 import {Heading} from "@/components/ui/heading";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
+import {DataTable} from "@/components/ui/data-table";
+import {BillboardColumn, columns} from "./columns";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+    data: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({data}) => {
     const router = useRouter();
     const params = useParams();
 
@@ -15,7 +22,7 @@ export const BillboardClient = () => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title="Billboards (0)"
+                    title={`Billboards (${data.length})`}
                     description="Manage billboards for your store"
                 />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
@@ -24,6 +31,7 @@ export const BillboardClient = () => {
                 </Button>
             </div>
             <Separator/>
+            <DataTable columns={columns} data={data} searchKey="label" />
         </>
     );
 };
