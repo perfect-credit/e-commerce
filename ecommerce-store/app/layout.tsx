8e97eb0e753718e6aa0807fe86b25957d953b19c
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2023-12-02 19:29:33 +0500

    add => main ecommerce website

---
+++ ecommerce-store/app/layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import type { Metadata } from 'next'
+import { Urbanist } from 'next/font/google'
+
+import Footer from '@/components/footer';
+
+import './globals.css'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Alpha Store ⚡',
+  description: 'E-commerce store',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
