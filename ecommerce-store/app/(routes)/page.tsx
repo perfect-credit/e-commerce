08fab8eb79e7591b4c7d9d7007d13c3778e74ac1
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2023-12-09 10:08:00 +0500

    add features => main ecommerce website

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,16 +1,22 @@
+import getProducts from "@/actions/get-products";
+import getBillboard from "@/actions/get-billboard";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
-import getBillboard from "@/actions/get-billboard";
+import ProductList from "@/components/product-list";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+    const products = await getProducts({isFeatured: "true"});
     const billboard = await getBillboard("affe646e-b6e1-46bc-9c36-48a84a97b491");
 
     return (
         <Container>
             <div className="space-y-10 pb-10">
-                <Billboard data={billboard} />
+                <Billboard data={billboard}/>
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title={"Featured Products"} products={products}/>
+                </div>
             </div>
         </Container>
     );
