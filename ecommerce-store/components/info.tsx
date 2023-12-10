7b055c2a0e5d7a8c0b51e06835b08ff7984ec951
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2023-12-10 20:02:57 +0500

    [add]: product individual page

--- ecommerce-store/components/info.tsx
+++ ecommerce-store/components/info.tsx
@@ -17,10 +17,10 @@ const Info: React.FC<InfoProps> = ({ data }) => {
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
             <div className="mt-3 flex items-end justify-between">
                 <p className="text-2xl text-gray-900">
-                    <Currency value={data?.price} />
+                    <Currency value={data?.price}/>
                 </p>
             </div>
-            <hr className="my-4" />
+            <hr className="my-4"/>
             <div className="flex flex-col gap-y-6">
                 <div className="flex items-center gap-x-4">
                     <h3 className="font-semibold text-black">Size:</h3>
@@ -30,7 +30,8 @@ const Info: React.FC<InfoProps> = ({ data }) => {
                 </div>
                 <div className="flex items-center gap-x-4">
                     <h3 className="font-semibold text-black">Colour:</h3>
-                    <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
+                    <div className="h-6 w-6 rounded-full border border-gray-600"
+                         style={{backgroundColor: data?.color?.value}}/>
                     <div>
                         {data?.color?.name}
                     </div>
@@ -39,7 +40,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
             <div className="mt-10 flex items-center gap-x-3">
                 <Button className="flex items-center gap-x-3">
                     Add to Cart
-                    <ShoppingCart />
+                    <ShoppingCart/>
                 </Button>
             </div>
         </div>
--- ecommerce-store/components/main-nav.tsx
+++ ecommerce-store/components/main-nav.tsx
@@ -14,7 +14,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
     const pathname = usePathname();
 
     const routes = data.map((route) => ({
-        href: `category/${route.id}`,
+        href: `/category/${route.id}`,
         label: route.name,
         active: pathname === `category/${route.id}`,
     }));
@@ -23,7 +23,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
         <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
             {routes.map((route) => (
                 <Link
-                    key={route.href}
+                    key={route.label}
                     href={route.href}
                     className={cn(
                         "text-sm font-medium transition-colors hover:text-black",
