38006d8b37656ac967e420025809738e5049c67c
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2023-12-20 18:07:37 +0500

    [fix]: checkout-btn click

--- ecommerce-store/components/info.tsx
+++ ecommerce-store/components/info.tsx
@@ -6,12 +6,19 @@ import {ShoppingCart} from "lucide-react";
 import {Product} from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+    const cart = useCart();
+
+    const onAddToCart = () => {
+        cart.addItem(data);
+    };
+
     return (
         <div>
             <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -38,7 +45,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
                 </div>
             </div>
             <div className="mt-10 flex items-center gap-x-3">
-                <Button className="flex items-center gap-x-3">
+                <Button onClick={onAddToCart} className="flex items-center gap-x-3">
                     Add to Cart
                     <ShoppingCart/>
                 </Button>
